9701129794ca1d871af97b0656e043c0a040eff1
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-05-09 23:55:56 +0600

    another update

--- src/pages/home/news/LatestNews.vue
+++ src/pages/home/news/LatestNews.vue
@@ -1,11 +1,21 @@
 <template>
  <div>
   <v-container>
-   <h3>Our Latest News</h3>
+   <div class="latestNewsDiv">
+    <h3 class="newHeading">Our Latest News</h3>
+   </div>
   </v-container>
  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.latestNewsDiv {
+ min-height: 505px;
+ background-color: antiquewhite;
+}
+.newHeading {
+ text-align: center;
+}
+</style>
