63ae4f63b9f7712ac1a871eb8f74a84b31cc0d20
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-06-05 17:49:47 +0600

    update to grid system for secial doctor

--- src/pages/home/ourSpecialist/OurSpecialist.vue
+++ src/pages/home/ourSpecialist/OurSpecialist.vue
@@ -1,57 +1,60 @@
-<!-- <template>
- <div class="ourSpecialist">
-  <v-container>
+<template>
+ <div class="">
+  <v-container style="max-width: 1400px; margin: 0 auto">
    <h5 class="ourSecialistTag"># Meet Our Specialists</h5>
    <h2 class="ourSpecialistHeading">
     Connect with our expert specialists<br />
     dedicated to your care
    </h2>
-   <div class="ourSpecialistDiv">
-    <div style="display: inline-block; padding: 2px">
-     <div class="specialDoctorImgDiv">
-      <img src="@/assets/image/ourSpecialist/team1-1.jpg" alt="doctor" />
-      <div class="doctorLindedinAccIconDiv">
-       <v-icon class="doctorLindedinAccIcon">mdi-linkedin</v-icon>
+   <v-row>
+    <v-col cols="12" sm="6" md="3" lg="3">
+     <div class="doctorimagediv">
+      <img src="@/assets/image/ourSpecialist/team1-1.jpg" alt="doctor" class="responsive-img" />
+      <div class="doctorLindedinIconDiv">
+       <v-icon class="doctorLindedinIcon">mdi-linkedin</v-icon>
       </div>
      </div>
      <h3 style="text-align: center; margin-top: 40px">Dr. Addison Patel</h3>
      <h5 style="text-align: center">Chief Executive Officer</h5>
-    </div>
+    </v-col>
 
-    <div style="display: inline-block; padding: 2px">
-     <div class="specialDoctorImgDiv">
-      <img src="@/assets/image/ourSpecialist/team2-1.jpg" alt="doctor" />
-      <div class="doctorLindedinAccIconDiv">
-       <v-icon class="doctorLindedinAccIcon">mdi-linkedin</v-icon>
+    <v-col cols="12" sm="6" md="3" lg="3">
+     <div class="doctorimagediv">
+      <img src="@/assets/image/ourSpecialist/team2-1.jpg" alt="doctor" class="responsive-img" />
+      <div class="doctorLindedinIconDiv">
+       <v-icon class="doctorLindedinIcon">mdi-linkedin</v-icon>
       </div>
      </div>
      <h3 style="text-align: center; margin-top: 40px">Dr. Emily Carter</h3>
      <h5 style="text-align: center">Chief Medical Officer</h5>
-    </div>
+    </v-col>
 
-    <div style="display: inline-block; padding: 2px">
-     <div class="specialDoctorImgDiv">
-      <img src="@/assets/image/ourSpecialist/team3-1.jpg" alt="doctor" />
-      <div class="doctorLindedinAccIconDiv">
-       <v-icon class="doctorLindedinAccIcon">mdi-linkedin</v-icon>
+    <v-col cols="12" sm="6" md="3" lg="3">
+     <div class="doctorimagediv">
+      <img src="@/assets/image/ourSpecialist/team3-1.jpg" alt="doctor" class="responsive-img" />
+      <div class="doctorLindedinIconDiv">
+       <v-icon class="doctorLindedinIcon">mdi-linkedin</v-icon>
       </div>
      </div>
      <h3 style="text-align: center; margin-top: 40px">Dr. Michael Thompson</h3>
      <h5 style="text-align: center">Chief Operating Officer</h5>
-    </div>
+    </v-col>
 
-    <div style="display: inline-block; padding: 2px">
-     <div class="specialDoctorImgDiv">
-      <img src="@/assets/image/ourSpecialist/team4-1.jpg" alt="doctor" />
-      <div class="doctorLindedinAccIconDiv">
-       <v-icon class="doctorLindedinAccIcon">mdi-linkedin</v-icon>
+    <v-col cols="12" sm="6" md="3" lg="3">
+     <div class="doctorimagediv">
+      <img src="@/assets/image/ourSpecialist/team4-1.jpg" alt="doctor" class="responsive-img" />
+      <div class="doctorLindedinIconDiv">
+       <v-icon class="doctorLindedinIcon">mdi-linkedin</v-icon>
       </div>
      </div>
      <h3 style="text-align: center; margin-top: 40px">Dr. Sarah Nguyen</h3>
      <h5 style="text-align: center">Chief Growth Officer</h5>
-    </div>
-   </div>
-   <p style="text-align: center">Click here to see All Our Specialists</p>
+    </v-col>
+   </v-row>
+
+   <p style="text-align: center; margin-top: 40px">
+    Click here to see All Our <span class="specialistSpan">Specialists</span>
+   </p>
   </v-container>
  </div>
 </template>
@@ -59,9 +62,6 @@
 <script setup lang="ts"></script>
 
 <style scoped>
-.ourSpecialist {
- min-height: 90vh;
-}
 .ourSecialistTag {
  text-align: center;
  font-family: 'sans-serif';
@@ -73,29 +73,31 @@
  font-size: clamp(1.4rem, 2.5vw, 2rem);
  max-width: 600px;
  margin: 0 auto;
+ margin-bottom: 50px;
  text-align: center;
  line-height: 1.2;
  word-break: break-word;
 }
-/* ------------------------- */
-.ourSpecialistDiv {
- display: grid;
- grid-template-columns: auto auto auto auto;
- justify-content: space-between;
- margin: 40px auto;
-}
-/* ------------------------- */
-.specialDoctorImgDiv {
- width: 270px;
- height: 340px;
+.doctorimagediv {
  position: relative;
 }
-.specialDoctorImgDiv img {
- width: 270px;
- height: 340px;
- border-radius: 20px;
+.responsive-img {
+ max-width: 100%;
+ height: auto;
+ border-radius: 10px;
 }
-.doctorLindedinAccIcon {
+.doctorLindedinIconDiv {
+ /* display: block; */
+ /* display: inline-block; */
+ padding: 8px;
+ background: rgb(255, 255, 255);
+ border-radius: 50%;
+ position: absolute;
+ bottom: -35px;
+ left: 50%;
+ transform: translateX(-50%);
+}
+.doctorLindedinIcon {
  font-size: 18px;
  padding: 23px;
  border-radius: 50%;
@@ -103,71 +105,14 @@
  color: white;
  cursor: pointer;
 }
-.doctorLindedinAccIconDiv {
- display: block;
- padding: 8px;
- background: rgb(255, 255, 255);
- display: inline-block;
- border-radius: 50%;
- position: absolute;
- bottom: -35px;
- left: 50%;
- transform: translateX(-50%);
+.specialistSpan {
+ color: rgb(21, 199, 89);
+ cursor: pointer;
+ text-decoration: underline;
+ font-weight: 600;
+ transition: 0.3s;
 }
-/* -------------------------- */
-@media (max-width: 1279px) {
- .ourSpecialistDiv {
-  display: grid;
-  grid-template-columns: auto auto;
-  justify-content: space-around;
-  gap: 20px;
-  margin: 40px auto;
- }
-}
-@media (max-width: 610px) {
- .specialDoctorImgDiv {
-  width: 320px;
-  height: 390px;
- }
- .specialDoctorImgDiv img {
-  width: 320px;
-  height: 390px;
- }
- /* ----------------------- */
- .ourSpecialistDiv {
-  display: grid;
-  grid-template-columns: auto;
-  /* justify-content: space-around; */
-  gap: 25px;
-  margin: 40px auto;
- }
-}
-</style> -->
-
-<template>
- <v-container class="masud">
-  <!-- <p>Left Column</p> -->
-  <v-row>
-   <p>Left Column</p>
-   <p>Left Column</p>
-   <p>Left Column</p>
-   <p>Left Column</p>
-  </v-row>
-  <!-- <v-row>
-   <v-col cols="12" md="6">
-    <p>Left Column</p>
-   </v-col>
-   <v-col cols="12" md="6">
-    <p>Right Column</p>
-   </v-col>
-  </v-row> -->
- </v-container>
-</template>
-
-<style scoped>
-.masud {
- background-color: rgb(153, 88, 88);
- height: 500px;
- width: 510px;
+.specialistSpan:hover {
+ color: rgb(21, 98, 199);
 }
 </style>
