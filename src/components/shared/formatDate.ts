15c4d7a7abd0e8ca898da2c24aca765a51bf4b81
Abdullah Al Masud <abdullahalmasud772@gmail.com>
2025-12-02 23:48:37 +0600

    another update

--- src/components/shared/formatDate.ts
+++ src/components/shared/formatDate.ts
@@ -9,3 +9,14 @@ export const formatDate = (dateString: string) => {
   // minute: 'numeric',
  })
 }
+export const formatDateWithHandM = (dateString: string) => {
+ if (!dateString) return 'N/A'
+ const date = new Date(dateString)
+ return date.toLocaleDateString('en-US', {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+ })
+}
